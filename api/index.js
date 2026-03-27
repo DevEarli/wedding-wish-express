36188a57637cffab50b6c9ac79b398cb2bebafc5
Synexis Microsystem <isa.wijaya@samudrainovasi.co.id>
2026-03-28 02:52:41 +0700

    Nambahin dikit

--- api/index.js
+++ api/index.js
@@ -2,43 +2,41 @@ const express = require("express");
 const cors = require("cors");
 const supabase = require("../lib/supabase.js");
 
+const port = 5000
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 // GET wishes
 app.get("/api/wishes", async (req, res) => {
   const { applicationId } = req.query;
-  console.log(applicationId);
   if (!applicationId) {
     return res.status(400).json({ error: "applicationId required" });
   }
-
-  
   const { data, error } = await supabase
     .from("wishes")
-    .select("*")
+    .select(`
+      *,
+      guests (
+        guest_name,
+        partner_name
+      )
+    `)
     .eq("application_id", applicationId)
     .order("created_at", { ascending: false })
     .limit(20);
-
   if (error) {
     return res.status(500).json({ error: error.message });
   }
-
   res.json(data);
 });
 
 // POST wish
 app.post("/api/wishes", async (req, res) => {
   const { applicationId, guestId, message, attendance } = req.body;
-  console.log(req.body);
-
   if (!applicationId || !guestId || !message) {
     return res.status(400).json({ error: "Invalid payload" });
   }
-
   const { error } = await supabase.from("wishes").upsert({
     id: crypto.randomUUID(),
     application_id: applicationId,
@@ -48,22 +46,17 @@ app.post("/api/wishes", async (req, res) => {
   }, {
     onConflict: "guest_id"
   });
-
-
   if (error) {
     return res.status(500).json({ error: error.message });
   }
-
   res.json({ success: true });
 });
 
 app.get("/api/guest", async (req, res) => {
   const { id } = req.query;
-
   if (!id) {
     return res.status(400).json({ error: "id required" });
   }
-
   const { data, error } = await supabase
     .from("guests")
     .select("*")
@@ -73,25 +66,9 @@ app.get("/api/guest", async (req, res) => {
   if (error) {
     return res.status(500).json({ error: error.message });
   }
-
   res.json(data);
 });
-// app.listen(5000,() => {
-//     console.log("runn");
-// });
 
-// ❗ PENTING: export app (bukan listen)
-// export default app;
-
-app.listen(5000,() => {
-  console.log("listennnn");
+app.listen(port, () => {
+  console.log("Listening on Port " + port);
 });
-// module.exports = app;/
-
-
-// export default function handler(req, res) {
-//   res.status(200).json({
-//     method: req.method,
-//     message: "wishes api works"
-//   });
-// }
